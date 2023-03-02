011940b19c1677fa7718029efaaf54bffb0fb35c
charlie foulk <charlesfoulk@csus.edu>
2023-03-01 22:13:29 -0800

    status bar scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",            "sb-cputemps",	           5,		0},
+	{"",            "sb-internet",	           5,		0},
+	{"",            "sb-volume",	           0,		15},
+
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
